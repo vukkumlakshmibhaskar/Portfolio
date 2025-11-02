7985717b868b0ed23ac0ea49e4213edf4c098175
vukkumlakshmibhaskar <115701463+vukkumlakshmibhaskar@users.noreply.github.com>
2025-11-02 11:32:42 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/BhaskarResumeUpdated.pdf";
+import pdf from "../../Assets/../Assets/bhaskar_resume (1).pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
